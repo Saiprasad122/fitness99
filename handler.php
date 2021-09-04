2f27559e36f6877914223cb35730362663b435df
Rajesh <=>
2021-09-04 16:34:10 +0530

    added docker file

---
+++ handler.php
@@ -0,0 +1,16 @@
+<?php
+ini_set('allow_url_fopen',1);
+switch (@parse_url($_SERVER['REQUEST_URI'])['path']) {
+    case '/':
+        require 'index.php';
+        break;
+    case '/index.php':
+        require 'index.php';
+        break;
+    case '/index':
+        require 'index.php';
+        break;
+    default:
+        http_response_code(404);
+        exit('Not Found');
+}
