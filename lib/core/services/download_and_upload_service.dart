15bb8c2f36bde2bca667b2e2879bdd2fb6215070
Atishchandra Manala <43942271+rockz1999@users.noreply.github.com>
2021-12-15 21:22:43 +0530

    Download isolate (#2)
    * upload function written
    
    * upload function written
    
    * added download service
    
    * added download service
    
    Co-authored-by: Atish Manala <pro.atish.31@gmail.com>

--- lib/core/services/download_and_upload_service.dart
+++ lib/core/services/download_and_upload_service.dart
@@ -1,28 +1,38 @@
 import 'dart:io';
 
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:fitness_99/core/services/get_directories.dart';
 import 'package:fitness_99/global/router/views.export.dart';
-import 'package:http/http.dart' as http;
+// import 'package:http/http.dart' as http;
 
 class DownloadAndUploadService extends GetxController {
-  // Future<void> downloadImageFromFirebase(Reference ref) async {
-  //   final String url = await ref.getDownloadURL();
-  //   final String name = await ref.name;
+  Future<bool> downloadImageFromFirebase(String imageName, int groupId,
+      Function(double progressPercent)? progressListener) async {
+    final directories = Get.find<DirectoriesService>();
+    final ref = FirebaseStorage.instance.ref(imageName);
+    // final String url = await ref.getDownloadURL();
+    final String name = await ref.name;
 
-  //   final http.Response downloadData = await http.get(
-  //     Uri.parse(url),
-  //   );
+    // final http.Response downloadData = await http.get(
+    //   Uri.parse(url),
+    // );
 
-  //   final File tempFile = File('${systemTempDir.path}/tmp.jpg');
-  //   if (tempFile.existsSync()) {
-  //     await tempFile.delete();
-  //   }
-  //   await tempFile.create();
-  //   final DownloadTask task = ref.writeToFile(tempFile);
-  //   final int byteCount = task.snapshot.totalBytes;
-  //   var bodyBytes = downloadData.bodyBytes;
-  //   final String path = await ref.fullPath;
-  // }
+    final File tempFile = File(
+        '${directories.getDownloadDirectoryForimagesOfGroup(groupId)}/$name.jpg');
+    if (tempFile.existsSync()) {
+      await tempFile.delete();
+    }
+    await tempFile.create();
+    final downloadTask = ref.writeToFile(tempFile);
+    downloadTask.catchError(() {
+      return false;
+    });
+    downloadTask.asStream().listen((event) {
+      progressListener?.call((event.bytesTransferred / event.totalBytes) * 100);
+    });
+    await downloadTask;
+    return true;
+  }
 
   Future<String?> uploadImageToFirebase(int groupId, File file,
       Function(double progressPercent)? progressListener) async {
