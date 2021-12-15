4144f566022fe3c3a69db30ac6e6f8e8247f793b
Atish Manala <pro.atish.31@gmail.com>
2021-12-15 21:20:13 +0530

    added download service

--- lib/bindings/splash_screen_binding.dart
+++ lib/bindings/splash_screen_binding.dart
@@ -1,5 +1,7 @@
 import 'package:fitness_99/controllers/auth_screen_controller/splashscreen_controller.dart';
 import 'package:fitness_99/core/api/api_service.dart';
+import 'package:fitness_99/core/services/download_and_upload_service.dart';
+import 'package:fitness_99/core/services/get_directories.dart';
 import 'package:get/get.dart';
 
 class SplashScreenBinding implements Bindings {
@@ -7,5 +9,7 @@ class SplashScreenBinding implements Bindings {
   void dependencies() {
     Get.put<SplashViewController>(SplashViewController());
     Get.put<ApiService>(ApiService.create());
+    Get.put<DownloadAndUploadService>(DownloadAndUploadService());
+    Get.put<DirectoriesService>(DirectoriesService());
   }
 }
