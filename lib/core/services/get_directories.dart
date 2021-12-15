a99dd324542092fa43a535ed5ba53d2a543b57cb
Atishchandra Manala <43942271+rockz1999@users.noreply.github.com>
2021-12-15 21:07:43 +0530

    Download isolate (#1)
    * upload function written
    
    * upload function written
    
    Co-authored-by: Atish Manala <pro.atish.31@gmail.com>

---
+++ lib/core/services/get_directories.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+import 'package:fitness_99/global/router/views.export.dart';
+import 'package:path_provider/path_provider.dart';
+
+class Directories extends GetxController {
+  Future<Directory> getDownloadDirectoryForimagesOfGroup(int groupId) async {
+    final dir = await getApplicationDocumentsDirectory();
+    return Directory(dir.path + "assets/$groupId/images/");
+  }
+
+  Future<Directory> getDownloadDirectoryForvideosOfGroup(int groupId) async {
+    final dir = await getApplicationDocumentsDirectory();
+    return Directory(dir.path + "assets/$groupId/videos/");
+  }
+
+  Future<Directory> getDownloadDirectoryForfilesOfGroup(int groupId) async {
+    final dir = await getApplicationDocumentsDirectory();
+    return Directory(dir.path + "assets/$groupId/files/");
+  }
+}
