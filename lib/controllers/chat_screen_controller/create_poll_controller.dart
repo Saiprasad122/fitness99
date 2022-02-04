d5d17853ccc891ef3f668ae527e698768871f5a6
Saiprasd122 <prasadyelagandula@gmail.com>
2022-02-05 03:56:54 +0530

    poll UI in progress

--- lib/controllers/chat_screen_controller/create_poll_controller.dart
+++ lib/controllers/chat_screen_controller/create_poll_controller.dart
@@ -1,3 +1,8 @@
 import 'package:fitness_99/global/router/views.export.dart';
+import 'package:flutter/cupertino.dart';
 
-class CreatePollController extends GetxController {}
+class CreatePollController extends GetxController {
+  final questionTED = TextEditingController();
+  final List<TextEditingController> controllerList = [];
+  final questionErrText = [];
+}
--- lib/views/chat_screen/chat_screen_tabs/poll_view/create_poll_view.dart
+++ lib/views/chat_screen/chat_screen_tabs/poll_view/create_poll_view.dart
@@ -1,9 +1,11 @@
+import 'package:fitness_99/controllers/chat_screen_controller/create_poll_controller.dart';
 import 'package:fitness_99/global/utils/fontsAndSizes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class CreatePollView extends StatelessWidget {
-  const CreatePollView({Key? key}) : super(key: key);
+  final controller = Get.put(CreatePollController());
+  CreatePollView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +22,53 @@ class CreatePollView extends StatelessWidget {
           icon: Icon(
             Icons.arrow_back_ios_new,
             color: Colors.black,
+          ),
+        ),
+      ),
+      body: Obx(
+        () => Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Question',
+                      style: TextStyles.sgproMedium.f24.greyMid,
+                    ),
+                    const SizedBox(height: 10),
+                    TextField(
+                      controller: controller.questionTED,
+                      style: TextStyles.sgproRegular.f20,
+                      keyboardType: TextInputType.multiline,
+                      maxLines: 5,
+                      maxLength: 250,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          gapPadding: 2,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        hintText: 'Enter Question here...',
+                        hintStyle: TextStyles.sgproRegular.f20.greyMid,
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey),
+                          gapPadding: 2,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    Text(
+                      'data',
+                      style: TextStyles.sgproRegular.f16.error,
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
         ),
       ),
