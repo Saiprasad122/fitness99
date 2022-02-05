3da1f4ea806810a427376812efc3aebcc123b9b5
Atishchandra Manala <43942271+rockz1999@users.noreply.github.com>
2022-02-06 00:57:50 +0530

    added create poll view

--- lib/controllers/chat_screen_controller/create_poll_controller.dart
+++ lib/controllers/chat_screen_controller/create_poll_controller.dart
@@ -1,8 +1,68 @@
 import 'package:fitness_99/global/router/views.export.dart';
+import 'package:fitness_99/global/widgets/custom_snackbar.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class CreatePollController extends GetxController {
   final questionTED = TextEditingController();
-  final List<TextEditingController> controllerList = [];
-  final questionErrText = [];
+  final optionControllerList = <TextEditingController>[
+    TextEditingController(),
+    TextEditingController(),
+  ].obs;
+  final questionErrText = ''.obs;
+  final optionErrText = ''.obs;
+  final isOptionListValid = true.obs;
+
+  void removeElement(int index) {
+    optionControllerList.removeAt(index);
+  }
+
+  void addElement() {
+    if (optionControllerList.length < 4) {
+      optionControllerList.add(TextEditingController());
+    } else {
+      Fluttertoast.showToast(msg: 'Only four options can be added');
+    }
+  }
+
+  bool validateQuestionTED() {
+    if (questionTED.text.isEmpty) {
+      questionErrText.value = 'Enter question';
+      return false;
+    } else {
+      questionErrText.value = '';
+      return true;
+    }
+  }
+
+  bool validateOptions() {
+    for (int i = 0; i < optionControllerList.length; i++) {
+      if (optionControllerList[i].text.isEmpty) {
+        isOptionListValid.value = false;
+        optionErrText.value = 'Enter all options';
+        break;
+      } else {
+        optionErrText.value = '';
+        isOptionListValid.value = true;
+      }
+    }
+    return isOptionListValid.value;
+  }
+
+  void onSubmit() {
+    validateQuestionTED();
+    validateOptions();
+    if (validateQuestionTED() && validateOptions()) {
+      if (optionControllerList.length < 2) {
+        customSnackBar(
+          title: 'Two Options Complusory',
+          message: 'Add atlest two options',
+          isSuccess: false,
+        );
+      }
+      print('All right');
+    } else {
+      print('No alrigh');
+    }
+  }
 }
--- lib/views/chat_screen/chat_screen_tabs/poll_view/create_poll_view.dart
+++ lib/views/chat_screen/chat_screen_tabs/poll_view/create_poll_view.dart
@@ -1,5 +1,8 @@
 import 'package:fitness_99/controllers/chat_screen_controller/create_poll_controller.dart';
 import 'package:fitness_99/global/utils/fontsAndSizes.dart';
+import 'package:fitness_99/global/widgets/custom_buttom_button.dart';
+import 'package:fitness_99/global/widgets/custom_button.dart';
+import 'package:fitness_99/global/widgets/custom_profile_textField.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -26,50 +29,105 @@ class CreatePollView extends StatelessWidget {
         ),
       ),
       body: Obx(
-        () => Padding(
-          padding: const EdgeInsets.all(15),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Question',
-                      style: TextStyles.sgproMedium.f24.greyMid,
-                    ),
-                    const SizedBox(height: 10),
-                    TextField(
-                      controller: controller.questionTED,
-                      style: TextStyles.sgproRegular.f20,
-                      keyboardType: TextInputType.multiline,
-                      maxLines: 5,
-                      maxLength: 250,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          gapPadding: 2,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        hintText: 'Enter Question here...',
-                        hintStyle: TextStyles.sgproRegular.f20.greyMid,
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey),
-                          gapPadding: 2,
-                          borderRadius: BorderRadius.circular(10),
+        () => Column(
+          children: [
+            Expanded(
+              child: ListView(
+                physics: BouncingScrollPhysics(),
+                padding: const EdgeInsets.all(15),
+                children: [
+                  ListView(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    children: [
+                      Text(
+                        'Question',
+                        style: TextStyles.sgproMedium.f24.greyMid,
+                      ),
+                      const SizedBox(height: 10),
+                      TextField(
+                        controller: controller.questionTED,
+                        style: TextStyles.sgproRegular.f20,
+                        keyboardType: TextInputType.multiline,
+                        maxLines: 5,
+                        maxLength: 250,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            gapPadding: 2,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          hintText: 'Enter Question here...',
+                          hintStyle: TextStyles.sgproRegular.f20.greyMid,
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey),
+                            gapPadding: 2,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
                         ),
                       ),
+                      const SizedBox(height: 5),
+                      Text(
+                        controller.questionErrText.value,
+                        style: TextStyles.sgproRegular.f16.error,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  ListView.separated(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemBuilder: (context, i) {
+                      return Row(
+                        children: [
+                          Expanded(
+                            child: CustomProfileTextField(
+                              displayText: 'Option${i + 1}',
+                              hintText: 'Enter option${i + 1}',
+                              textEditingController:
+                                  controller.optionControllerList[i],
+                            ),
+                          ),
+                          i == controller.optionControllerList.length - 1
+                              ? i == 3
+                                  ? IconButton(
+                                      onPressed: () =>
+                                          controller.removeElement(3),
+                                      icon: Icon(Icons.close),
+                                      padding: const EdgeInsets.all(0),
+                                    )
+                                  : IconButton(
+                                      onPressed: controller.addElement,
+                                      icon: Icon(Icons.add),
+                                      padding: const EdgeInsets.all(0),
+                                    )
+                              : IconButton(
+                                  onPressed: () => controller.removeElement(i),
+                                  icon: Icon(Icons.close),
+                                  padding: const EdgeInsets.all(0),
+                                ),
+                        ],
+                      );
+                    },
+                    separatorBuilder: (context, i) =>
+                        const SizedBox(height: 10),
+                    itemCount: controller.optionControllerList.length,
+                  ),
+                  const SizedBox(height: 10),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Text(
+                      controller.optionErrText.value,
+                      style: TextStyles.sgproRegular.f20.error,
                     ),
-                    const SizedBox(height: 5),
-                    Text(
-                      'data',
-                      style: TextStyles.sgproRegular.f16.error,
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
+                  )
+                ],
+              ),
+            ),
+            CustomBottomButton(
+              text: 'Submit',
+              onTap: controller.onSubmit,
+            )
+          ],
         ),
       ),
     );
